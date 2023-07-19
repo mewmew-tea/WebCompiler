f14a8acfbdb36028f06eddf6379263f405c5c4ab
mewmew-tea <kd1285579@st.kobedenshi.ac.jp>
2023-07-19 11:55:59 +0900

    Make it possible to run multiple test cases

--- src/index.js
+++ src/index.js
@@ -67,71 +67,92 @@ document.getElementById('buttonCompile').addEventListener('click', async (e) => 
     // ボタンを無効化
     buttonCompile.disabled = true;
 
-    var id;     // コンパイル・実行のリクエストID
-    const input = '100 200 300';    // 標準入力
-    const except = '600\n';           // 期待する標準出力
+    const testCases = [['100 200 300', '600\n'],
+    ['1 2 3', '6\n'],
+    ['10 20 30', '60\n'],
+    ['1000 2000 3000', '6000\n'],];
 
-    //-----------------------------
-    // コンパイル・実行のリクエスト
-    //-----------------------------
-    // ※無名関数を即時実行して、変数名被りを防ぐ（C++でスコープ使うのと同じ目的）
-    await (async () => {
-        const url = 'http://api.paiza.io/runners/create';
+    var currentCaseCnt = 1;
+    await testCases.forEach(async function (testCase) {
 
-        console.log(editor.getModel().getValue());
+        var id;     // コンパイル・実行のリクエストID
+        const input = testCase[0];    // 標準入力
+        const except = testCase[1];           // 期待する標準出力
+        // 以前のPDFを削除
+        const outputsContainer = document.getElementById('outputs-container');
+        while (outputsContainer.firstChild) {
+            outputsContainer.removeChild(outputsContainer.firstChild);
+        }
 
-        const data = {
-            'source_code': editor.getModel().getValue(),
-            'language': 'cpp',
-            'input': input,
-            'api_key': 'guest',
-        };
+        //-----------------------------
+        // コンパイル・実行のリクエスト
+        //-----------------------------
+        // ※無名関数を即時実行して、変数名被りを防ぐ（C++でスコープ使うのと同じ目的）
+        await (async () => {
+            const url = 'http://api.paiza.io/runners/create';
 
-        // コンパイル・実行
-        // ソースコードやコマンドライン引数などの情報を送信
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-        // if (response.ok) {
-        //     console.log("正常です");
-        // }
-        const responseData = await response.json();
-        id = responseData.id;
-        console.log(id);
-    })();
+            console.log(editor.getModel().getValue());
+
+            const data = {
+                'source_code': editor.getModel().getValue(),
+                'language': 'cpp',
+                'input': input,
+                'api_key': 'guest',
+            };
+
+            // コンパイル・実行
+            // ソースコードやコマンドライン引数などの情報を送信
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+            // if (response.ok) {
+            //     console.log("正常です");
+            // }
+            const responseData = await response.json();
+            id = responseData.id;
+            console.log(id);
+        })();
 
 
-    await sleep(3000); // 3秒待つ
+        await sleep(3000); // 3秒待つ
 
-    //-----------------------------
-    // コンパイル・実行結果の取得
-    //-----------------------------
-    // ※無名関数を即時実行して、変数名被りを防ぐ（C++でスコープ使うのと同じ目的）
-    await (async () => {
-        const url = `http://api.paiza.io/runners/get_details?id=${id}&api_key=guest`;
+        //-----------------------------
+        // コンパイル・実行結果の取得
+        //-----------------------------
+        // ※無名関数を即時実行して、変数名被りを防ぐ（C++でスコープ使うのと同じ目的）
+        await (async () => {
+            const url = `http://api.paiza.io/runners/get_details?id=${id}&api_key=guest`;
 
-        // 出力結果をHTTP通信（GET）で取得
-        const response = await fetch(url);
-        const responseData = await response.json();
-        console.log(responseData);
-        console.log(responseData.stdout);
+            // 出力結果をHTTP通信（GET）で取得
+            const response = await fetch(url);
+            const responseData = await response.json();
+            console.log(responseData);
+            console.log(responseData.stdout);
 
-        var buildErrorMsg = responseData.build_stderr;
+            var buildErrorMsg = responseData.build_stderr;
 
-        var isCollect = responseData.build_result == 'success' && responseData.stdout == except;
+            var isCollect = responseData.build_result == 'success' && responseData.stdout == except;
 
-        // 出力結果を表示
-        document.getElementById('output').innerText = 
-`${isCollect ? '正解！' : '不正解…' }
+            // 出力結果を表示
+            const outputElement = document.createElement('div');
+            outputElement.style.border = 'solid 1px #ccc';
+            outputElement.innerText =
+                `【テストケース${currentCaseCnt}】${isCollect ? '正解！' : '不正解…'}
 （ビルド結果: ${responseData.build_result} ${buildErrorMsg} ）
 標準入力: ${input}
 標準出力: ${responseData.stdout}
 期待する出力: ${except}`;
-    })();
+
+            outputsContainer.appendChild(outputElement);
+            
+            currentCaseCnt++;
+        })();
+
+    });
 
     // ボタンを有効化
     buttonCompile.disabled = false;
